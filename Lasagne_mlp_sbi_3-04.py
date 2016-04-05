df50102289cda7324449e985a33c8eb1a4817b50
robin93 <robinsinghiitd@gmail.com>
2016-04-05 18:34:51 +0530

    Initial weights set from random normal to constant 1

--- Lasagne_mlp_sbi_3-04.py
+++ Lasagne_mlp_sbi_3-04.py
@@ -1,6 +1,7 @@
 import sys,os,time,theano,lasagne
 import numpy as np
 import theano.tensor as T
+from lasagne.init import Constant
 theano.config.optimizer='fast_compile'
 theano.config.exception_verbosity='high'
 
@@ -14,22 +15,24 @@ from lasagne.objectives import binary_crossentropy, aggregate
 from numpy import genfromtxt
 data = genfromtxt('SBI_processed_data.csv', delimiter=',',usecols=[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],skip_header=10)
 
-X_train, y_train,X_test, y_test = data[10:5500,:16],data[10:5500,16:],data[5500:,:16],data[5500:,16:]
-print X_train.shape
+X_train, y_train,X_test, y_test = data[4000:5500,:16],data[4000:5500,16:],data[5500:,:16],data[5500:,16:]
+# print X_train.shape
+# print y_test
 
 batch_size = 20
 n_training_batches = len(X_train)//batch_size 
 n_test_batches = len(X_test)//batch_size
 
 def build_mlp(input_var):
-    l_in = lasagne.layers.InputLayer((1,16), name='INPUT')
-    # l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=20, name = 'Hidden1')
+    l_in = lasagne.layers.InputLayer((1,16),W= Constant([1]),name='INPUT')
+    l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=30,W= Constant([1]),name = 'Hidden1')
+    # l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=30,nonlinearity=lasagne.nonlinearities.linear, name = 'Hidden1')
     # l_hid2 = lasagne.layers.DenseLayer(l_hid1, num_units=20, name = 'Hidden2')
-    l_out = lasagne.layers.DenseLayer(l_in, num_units=1,nonlinearity=lasagne.nonlinearities.rectify, name = 'OUTPUT')
+    l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,W= Constant([1]),nonlinearity=lasagne.nonlinearities.rectify, name = 'OUTPUT')
+    # l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,nonlinearity=lasagne.nonlinearities.leaky_rectify, name = 'OUTPUT')
+    # l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,nonlinearity=lasagne.nonlinearities.leaky_rectify, name = 'OUTPUT')
     return l_out
 
-# input_var = T.dvector('inputs')
-# target_var = T.dvector('targets')
 input_var = T.matrix('inputs')
 target_var = T.matrix('targets')
 
@@ -39,10 +42,10 @@ network = build_mlp(input_var)
 
 prediction = lasagne.layers.get_output(network,input_var, deterministic = True)
 loss = lasagne.objectives.squared_error(prediction, target_var)
-loss = aggregate(loss, mode='mean')
+loss = aggregate(loss, weights=2.9*target_var,mode='mean')
 
 params = lasagne.layers.get_all_params(network, trainable=True)
-updates = lasagne.updates.sgd(loss, params, learning_rate=0.001)
+updates = lasagne.updates.sgd(loss, params, learning_rate=0.00001)
 
 test_prediction = lasagne.layers.get_output(network,input_var, deterministic=True)
 test_loss = lasagne.objectives.squared_error(test_prediction,target_var)
@@ -57,31 +60,40 @@ print("Starting training...")
 for epoch in range(num_epochs):
     train_err = 0
     start_time = time.time()
-    # for i in range(len(X_train)):
-    #     train_err = train_err + train_fn(X_train[i],y_train[i])
-    #     print train_err
 
     for i in range(n_training_batches):
         train_err += train_fn(X_train[batch_size*i:batch_size*(i+1),],y_train[batch_size*i:batch_size*(i+1)])
-        print train_err
+    print train_err
     
     print("Epoch {} of {} took {:.3f}s".format(
         epoch + 1, num_epochs, time.time() - start_time))
 
 test_err = 0
 test_acc = 0
-# for i in range(len(X_test)):
-#     error,acc = val_fn(X_test[i],y_test[i])
-#     test_err += error
-#     test_acc += acc
-#     print test_err,test_acc
 
 for i in range(n_test_batches):
     # error,acc = val_fn(X_test[batch_size*i:batch_size*(i+1)],y_test[batch_size*i:batch_size*(i+1)])
     error = val_fn(X_test[batch_size*i:batch_size*(i+1)],y_test[batch_size*i:batch_size*(i+1)])
     test_err += error
     # test_acc += acc
-    print test_err,test_acc
+print "Test error",test_err#,test_acc
+
+print "Test predictions outputs",test_prediction
+print "parameter values",params
+
+print "Last layer weights:"
+print lasagne.layers.get_all_param_values(network)[-1]
+print lasagne.layers.get_all_param_values(network)[-2]
+print lasagne.layers.get_all_param_values(network)
+
+
+f_test = theano.function([input_var],test_prediction)
+print list(f_test(X_test))
+
+threshold = 1.3
+predictions = [1 if i[0] > threshold else 0 for i in list(f_test(X_test))]
+print predictions
+
 
 # for batch in iterate_minibatches(X_test, y_test, 20, shuffle=False):
 #     inputs, targets = batch
