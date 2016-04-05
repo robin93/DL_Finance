12e2356ac92a18d71bb7a00cfc82a609e678bbce
robin93 <robinsinghiitd@gmail.com>
2016-04-05 22:11:25 +0530

    the code discussed with Anirban and incorporates changes in non-linearity etc.

--- Lasagne_mlp_sbi_3-04.py
+++ Lasagne_mlp_sbi_3-04.py
@@ -24,11 +24,11 @@ n_training_batches = len(X_train)//batch_size
 n_test_batches = len(X_test)//batch_size
 
 def build_mlp(input_var):
-    l_in = lasagne.layers.InputLayer((1,16),W= Constant([1]),name='INPUT')
-    l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=30,W= Constant([1]),name = 'Hidden1')
+    l_in = lasagne.layers.InputLayer((1,16),name='INPUT')
+    l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=100,name = 'Hidden1')
     # l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=30,nonlinearity=lasagne.nonlinearities.linear, name = 'Hidden1')
     # l_hid2 = lasagne.layers.DenseLayer(l_hid1, num_units=20, name = 'Hidden2')
-    l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,W= Constant([1]),nonlinearity=lasagne.nonlinearities.rectify, name = 'OUTPUT')
+    l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,nonlinearity=lasagne.nonlinearities.sigmoid, name = 'OUTPUT')
     # l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,nonlinearity=lasagne.nonlinearities.leaky_rectify, name = 'OUTPUT')
     # l_out = lasagne.layers.DenseLayer(l_hid1, num_units=1,nonlinearity=lasagne.nonlinearities.leaky_rectify, name = 'OUTPUT')
     return l_out
@@ -36,13 +36,14 @@ def build_mlp(input_var):
 input_var = T.matrix('inputs')
 target_var = T.matrix('targets')
 
-num_epochs=10
+num_epochs=100
 
 network = build_mlp(input_var)
 
 prediction = lasagne.layers.get_output(network,input_var, deterministic = True)
 loss = lasagne.objectives.squared_error(prediction, target_var)
-loss = aggregate(loss, weights=2.9*target_var,mode='mean')
+# loss = aggregate(loss, weights=3.2*target_var,mode='mean')
+loss = aggregate(loss,mode='mean')
 
 params = lasagne.layers.get_all_params(network, trainable=True)
 updates = lasagne.updates.sgd(loss, params, learning_rate=0.00001)
@@ -65,8 +66,8 @@ for epoch in range(num_epochs):
         train_err += train_fn(X_train[batch_size*i:batch_size*(i+1),],y_train[batch_size*i:batch_size*(i+1)])
     print train_err
     
-    print("Epoch {} of {} took {:.3f}s".format(
-        epoch + 1, num_epochs, time.time() - start_time))
+    # print("Epoch {} of {} took {:.3f}s".format(
+    #     epoch + 1, num_epochs, time.time() - start_time))
 
 test_err = 0
 test_acc = 0
@@ -76,23 +77,52 @@ for i in range(n_test_batches):
     error = val_fn(X_test[batch_size*i:batch_size*(i+1)],y_test[batch_size*i:batch_size*(i+1)])
     test_err += error
     # test_acc += acc
-print "Test error",test_err#,test_acc
+# print "Test error",test_err#,test_acc
 
-print "Test predictions outputs",test_prediction
-print "parameter values",params
+# print "Test predictions outputs",test_prediction
+# print "parameter values",params
 
-print "Last layer weights:"
-print lasagne.layers.get_all_param_values(network)[-1]
-print lasagne.layers.get_all_param_values(network)[-2]
-print lasagne.layers.get_all_param_values(network)
+# print "Last layer weights:"
+# print lasagne.layers.get_all_param_values(network)[-1]
+# print lasagne.layers.get_all_param_values(network)[-2]
+# print lasagne.layers.get_all_param_values(network)
 
 
 f_test = theano.function([input_var],test_prediction)
-print list(f_test(X_test))
+# print list(f_test(X_test))
 
-threshold = 1.3
-predictions = [1 if i[0] > threshold else 0 for i in list(f_test(X_test))]
-print predictions
+threshold = 0.5
+prediction_list = [1 if i[0] > threshold else 0 for i in list(f_test(X_test))]
+# print prediction_list
+
+y_test_list = list()
+for item in list(y_test):
+    y_test_list.append(int(item[0]))
+print y_test_list
+
+# print "length of prediction list",len(prediction_list)
+# print "length of y_test",len(y_test_list)
+
+confusion_matrix_array = [0,0,0,0]
+for index in range(len(y_test_list)):
+    if y_test_list[index]==0:
+        if prediction_list[index]==0:
+            confusion_matrix_array[0] += 1
+        else:
+            confusion_matrix_array[2] += 1
+    else:
+        if prediction_list[index] == 1:
+            confusion_matrix_array[1] += 1
+        else:
+            confusion_matrix_array[3] += 1
+
+print "Threshold value",threshold
+print "Target value = 1 & Prediction = 1", confusion_matrix_array[1]
+print "Target value = 1 & Prediction = 0", confusion_matrix_array[3]
+print "Target value = 0 & Prediction = 0", confusion_matrix_array[0]
+print "Target value = 0 & Prediction = 1", confusion_matrix_array[2]
+
+
 
 
 # for batch in iterate_minibatches(X_test, y_test, 20, shuffle=False):
