aa14b42fbae0495ad83b6e2356bf4c621dfb6c12
robin93 <robinsinghiitd@gmail.com>
2016-05-06 16:22:16 +0530

    using cost sensitive learning

--- Lasagne_mlp_sbi_2class_predict.py
+++ Lasagne_mlp_sbi_2class_predict.py
@@ -5,34 +5,10 @@ import theano.tensor as T
 from lasagne.init import Constant
 theano.config.optimizer='fast_compile'
 theano.config.exception_verbosity='high'
-
-
-
 from lasagne.nonlinearities import softmax
 from lasagne.layers import InputLayer, DenseLayer, get_output
 from lasagne.updates import sgd, apply_momentum
 from lasagne.objectives import binary_crossentropy, aggregate
-
-from numpy import genfromtxt
-cols = [i for i in range(1,41)]
-raw_data = genfromtxt('SBI_lag_TI_rise_fall.csv', delimiter=',',usecols=cols,skip_header=20)
-
-
-unq_rise,unq_idx_rise = np.unique(raw_data[:,-2],return_inverse=True)
-unq_dip,unq_idx_dip = np.unique(raw_data[:,-1],return_inverse=True)
-unq_cnt_rise,unq_cnt_dip = np.bincount(unq_idx_rise),np.bincount(unq_idx_dip)
-cnt_rise_dip = np.max(unq_cnt_rise)-np.min(unq_cnt_dip)
-data_rise_dip = np.empty((cnt_rise_dip*3,) + raw_data.shape[1:],raw_data.dtype)
-intersectArray = np.intersect1d(np.where(unq_idx_dip==0)[0],np.where(unq_idx_rise==0)[0],assume_unique = True)
-rise_indices = np.where(unq_idx_rise==1)[0]
-dip_indices = np.where(unq_idx_dip==1)[0]
-sampled_indices_0 = np.random.choice(intersectArray,cnt_rise_dip)
-sampled_indices_rise = np.random.choice(rise_indices,cnt_rise_dip)
-sampled_indices_dip = np.random.choice(dip_indices,cnt_rise_dip)
-data_rise_dip[0:cnt_rise_dip] = raw_data[sampled_indices_0]
-data_rise_dip[cnt_rise_dip:cnt_rise_dip*2] = raw_data[sampled_indices_rise]
-data_rise_dip[cnt_rise_dip*2:cnt_rise_dip*3] = raw_data[sampled_indices_dip]
-data = data_rise_dip[np.argsort(data_rise_dip[:,0])]
 
 def iterate_minibatches(inputs,targets,batch_size,shuffle=True):
     assert len(inputs)==len(targets)
@@ -46,14 +22,55 @@ def iterate_minibatches(inputs,targets,batch_size,shuffle=True):
             excerpt = slice(start_idx,start_idx+batch_size)
         yield inputs[excerpt],targets[excerpt]
 
-X_train, y_train,X_test, y_test = data[9000:14500,1:38],data[9000:14500,38:],data[14500:,1:38],data[14500:,38:]
+def cost_sensitive_weights(target_values,a,b):
+    weight_array = np.empty((target_values.shape[0],target_values.shape[1]),dtype=float)
+    # weight_array = np.empty((50,2),dtype=float)
+    count = 0
+    for i in target_values:
+        if (i[0] == 1):
+            weight_array[count][0] = a
+            weight_array[count][1] = b
+        elif (i[1]==1):
+            weight_array[count][1] = a
+            weight_array[count][0] = b
+        else:
+            weight_array[count][0] = b
+            weight_array[count][1] = b
+        count += 1
+    return weight_array
 
-# print X_train
-# print y_train
+"""import data and convert to numpy array, segment into train and test"""
+from numpy import genfromtxt
+cols = [i for i in range(1,41)]
+raw_data = genfromtxt('SBI_lag_TI_rise_fall.csv', delimiter=',',usecols=cols,skip_header=20)
+data = raw_data
 
-with np.load('model.npz') as f:
-    param_values = [f['arr_%d'%i] for i in range(len(f.files))]
+print raw_data.shape
+X_train, y_train,X_test, y_test = data[3385:5385,1:38],data[3385:5385,38:],data[5385:,1:38],data[5385:,38:]
 
+# """oversampling the train dataset to treat class imbalance"""
+# unq_rise,unq_idx_rise = np.unique(raw_data[:,-2],return_inverse=True)
+# unq_dip,unq_idx_dip = np.unique(raw_data[:,-1],return_inverse=True)
+# unq_cnt_rise,unq_cnt_dip = np.bincount(unq_idx_rise),np.bincount(unq_idx_dip)
+# cnt_rise_dip = np.max(unq_cnt_rise)-np.min(unq_cnt_dip)
+# data_rise_dip = np.empty((cnt_rise_dip*3,) + raw_data.shape[1:],raw_data.dtype)
+# intersectArray = np.intersect1d(np.where(unq_idx_dip==0)[0],np.where(unq_idx_rise==0)[0],assume_unique = True)
+# rise_indices = np.where(unq_idx_rise==1)[0]
+# dip_indices = np.where(unq_idx_dip==1)[0]
+# sampled_indices_0 = np.random.choice(intersectArray,cnt_rise_dip)
+# sampled_indices_rise = np.random.choice(rise_indices,cnt_rise_dip)
+# sampled_indices_dip = np.random.choice(dip_indices,cnt_rise_dip)
+# data_rise_dip[0:cnt_rise_dip] = raw_data[sampled_indices_0]
+# data_rise_dip[cnt_rise_dip:cnt_rise_dip*2] = raw_data[sampled_indices_rise]
+# data_rise_dip[cnt_rise_dip*2:cnt_rise_dip*3] = raw_data[sampled_indices_dip]
+# data = data_rise_dip[np.argsort(data_rise_dip[:,0])]
+
+
+# X_train, y_train,X_test, y_test = data[9000:14500,1:38],data[9000:14500,38:],data[14500:,1:38],data[14500:,38:]
+
+
+
+"""building the network"""
 def build_mlp(input_var):
     l_in = lasagne.layers.InputLayer((1,37),name='INPUT')
     l_hid1 = lasagne.layers.DenseLayer(l_in, num_units=50,name = 'Hidden1')
@@ -66,20 +83,24 @@ def build_mlp(input_var):
 
 
 input_var,target_var = T.matrix('inputs'),T.matrix('targets')
-
-num_epochs=120
-
+num_epochs=50
 network = build_mlp(input_var)
-lasagne.layers.set_all_param_values(network,param_values)
-
+"""loading weight values from the previous model"""
+# """load layer parameters. To be used only when learning walk forward"""
+# with np.load('model.npz') as f:
+#     param_values = [f['arr_%d'%i] for i in range(len(f.files))]
+# lasagne.layers.set_all_param_values(network,param_values)
 prediction = lasagne.layers.get_output(network,input_var, deterministic = True)
 loss = lasagne.objectives.binary_crossentropy(prediction, target_var)
-# loss = aggregate(loss, weights=3.2*target_var,mode='mean')
-loss = aggregate(loss,mode='mean')
-
+# cost_sensitive_weights = [0.95 if i==1 else 0.05 for i in target_var]
+class_weights = np.empty((50,2),dtype=float)
+class_weights_global = class_weights
+# loss = aggregate(loss, weights=cost_sensitive_weights(target_var,0.5),mode='mean')
+# loss = aggregate(loss, weights=theano.shared(class_weights),mode='mean')
+loss = aggregate(loss, weights=theano.shared(class_weights_global),mode='normalized_sum')
+# loss = aggregate(loss,mode='mean')
 params = lasagne.layers.get_all_params(network, trainable=True)
 updates = lasagne.updates.sgd(loss, params, learning_rate=0.2)
-
 test_prediction = lasagne.layers.get_output(network,input_var, deterministic=True)
 test_loss = lasagne.objectives.binary_crossentropy(test_prediction,target_var)
 test_loss = test_loss.mean()
@@ -89,6 +110,7 @@ train_fn = theano.function([input_var, target_var],loss, updates=updates)
 # val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
 val_fn = theano.function([input_var, target_var],test_loss)
 
+"""Training"""
 print("Starting training...")
 count = 0
 count_list,train_err_list = list(),list()
@@ -96,6 +118,8 @@ for epoch in range(num_epochs):
     train_err = 0
     for batch in iterate_minibatches(X_train,y_train,50,shuffle=True):
         inputs,targets = batch
+        class_weights = cost_sensitive_weights(targets,10,1)
+        # print class_weights
         batch_error = train_fn(inputs,targets)
         train_err += batch_error
     count += 1
@@ -105,34 +129,33 @@ for epoch in range(num_epochs):
     print train_err
     # print "epoch number", epoch,"training error",train_err
 
-print "Last layer weights:"
-print "lasagne.layers.get_all_param_values(network)[-1]",lasagne.layers.get_all_param_values(network)[-1]
-print "shape",(lasagne.layers.get_all_param_values(network)[-1]).shape
-print "lasagne.layers.get_all_param_values(network)[-2]"#,lasagne.layers.get_all_param_values(network)[-2]
-print "shape",(lasagne.layers.get_all_param_values(network)[-2]).shape
-print "lasagne.layers.get_all_param_values(network)[-3]"#,lasagne.layers.get_all_param_values(network)[-3]
-print "shape",(lasagne.layers.get_all_param_values(network)[-3]).shape
-print "lasagne.layers.get_all_param_values(network)[-4]"#,lasagne.layers.get_all_param_values(network)[-4]
-print "shape",(lasagne.layers.get_all_param_values(network)[-4]).shape
-print "lasagne.layers.get_all_param_values(network)[-5]"#,lasagne.layers.get_all_param_values(network)[-3]
-print "shape",(lasagne.layers.get_all_param_values(network)[-5]).shape
-print "lasagne.layers.get_all_param_values(network)[-6]"#,lasagne.layers.get_all_param_values(network)[-4]
-print "shape",(lasagne.layers.get_all_param_values(network)[-6]).shape
+# print "Last layer weights:"
+# print "lasagne.layers.get_all_param_values(network)[-1]",lasagne.layers.get_all_param_values(network)[-1]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-1]).shape
+# print "lasagne.layers.get_all_param_values(network)[-2]"#,lasagne.layers.get_all_param_values(network)[-2]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-2]).shape
+# print "lasagne.layers.get_all_param_values(network)[-3]"#,lasagne.layers.get_all_param_values(network)[-3]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-3]).shape
+# print "lasagne.layers.get_all_param_values(network)[-4]"#,lasagne.layers.get_all_param_values(network)[-4]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-4]).shape
+# print "lasagne.layers.get_all_param_values(network)[-5]"#,lasagne.layers.get_all_param_values(network)[-3]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-5]).shape
+# print "lasagne.layers.get_all_param_values(network)[-6]"#,lasagne.layers.get_all_param_values(network)[-4]
+# print "shape",(lasagne.layers.get_all_param_values(network)[-6]).shape
 
 # np.savez('model.npz',*lasagne.layers.get_all_param_values(network))
 
 f_test = theano.function([input_var],test_prediction)
-# print list(f_test(X_test))
+print list(f_test(X_test))
 
-threshold = 0.25
+threshold = 0.055
 prediction_list_rise = [1 if i[0] > threshold else 0 for i in list(f_test(X_test))]
 prediction_list_dip = [1 if i[1] > threshold else 0 for i in list(f_test(X_test))]
 y_test_list_rise = [int(i[0]) for i in list(y_test)]
 y_test_list_dip = [int(i[1]) for i in list(y_test)]
 
 
-
-# print "percentiles", [np.percentile(prediction_list_rise,10*i) for i in range(10)]
+print "percentiles", [np.percentile(prediction_list_rise,10*i) for i in range(10)]
 
 from sklearn.metrics import confusion_matrix
 from terminaltables import AsciiTable
